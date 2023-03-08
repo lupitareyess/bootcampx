eabe130e98c6fac12334a0b163f183815145c6cf
Lupita Reyes <lupita.g.reyes@outlook.com>
2023-03-08 09:29:59 -0500

    JOIN tables

--- migrations/assignments_submissions.sql
+++ migrations/assignments_submissions.sql
@@ -15,4 +15,13 @@
 --   submission_date DATE
 -- );
 
-SELECT count(*) FROM assignment_submissions;
+-- SELECT count(*) FROM assignment_submissions;
+
+-- SELECT students.name as student_name, email, cohorts.name as cohort_name
+-- FROM students FULL OUTER JOIN cohorts ON cohort_id = cohorts.id;
+
+SELECT students.name, cohorts.name, cohorts.start_date as cohort_start_date, students.start_date as student_start_date
+FROM students 
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.start_date != students.start_date
+ORDER BY cohort_start_date;
