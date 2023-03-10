d0dd0c83f88e52ea5eb1b2f840c246d731459740
Lupita Reyes <lupita.g.reyes@outlook.com>
2023-03-10 03:12:15 -0500

    Install postgres and setup node app

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -10,15 +10,15 @@ const pool = new Pool({
 const args = process.argv[2];
 
 pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort_name
-FROM cohorts
-JOIN students ON cohort_id = cohorts.id
-where cohorts.name LIKE '%${args}%'
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+FROM students 
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE '%${process.argv[2]}%'
 LIMIT ${process.argv[3] || 5};
 `)
   .then(res => {
     res.rows.forEach(user => {
-      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort_name} cohort`)
+      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`)
     });
   }).catch(err => console.error('query error', err.stack));
 
